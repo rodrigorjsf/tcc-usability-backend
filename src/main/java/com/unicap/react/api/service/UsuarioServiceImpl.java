64ec3c597d8370fa7d2ac30ffdb7f8e84502309e
Rodrigo Franca <rodrigo.franca@flexpag.com>
2020-06-16 00:50:15 -0300

    Implementação de DTO para usuário. Ajuste na validação no cadastro de usuário.

--- src/main/java/com/unicap/react/api/service/UsuarioServiceImpl.java
+++ src/main/java/com/unicap/react/api/service/UsuarioServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 public class UsuarioServiceImpl implements UserDetailsService {
@@ -26,10 +27,11 @@ public class UsuarioServiceImpl implements UserDetailsService {
 
     @Transactional
     public Usuario salvar(Usuario usuario) {
-        Usuario user = usuarioRepository.findByEmail(usuario.getEmail()).orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado na base de dados."));
-        if (user.getEmail().trim().equals(usuario.getEmail().trim())){
+        Optional<Usuario> user = usuarioRepository.findByEmail(usuario.getEmail().trim());
+        if (user.isPresent()) {
             throw new UserAlreadyRegisteredException();
         }
+        usuario.setEmail(usuario.getEmail().trim());
         return usuarioRepository.save(usuario);
     }
 
