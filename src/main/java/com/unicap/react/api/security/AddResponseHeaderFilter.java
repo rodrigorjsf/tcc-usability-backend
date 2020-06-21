198e7d00d8046268392b5d7de18dd73c4a0f5e7b
rodrigo.franca <rodrigo_rjsf@hotmail.com>
2020-06-21 20:03:05 -0300

    Organização de código.

--- src/main/java/com/unicap/react/api/security/AddResponseHeaderFilter.java
+++ src/main/java/com/unicap/react/api/security/AddResponseHeaderFilter.java
@@ -16,8 +16,8 @@ public class AddResponseHeaderFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
         httpServletResponse.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
-        httpServletResponse.setHeader ("Access-Control-Request-Method", "POST, PUT, GET, OPTIONS, DELETE");
-        httpServletResponse.setHeader ("Access-Control-Max-Age", "3600");
+        httpServletResponse.setHeader (HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD, "POST, PUT, GET, OPTIONS, DELETE");
+        httpServletResponse.setHeader (HttpHeaders.ACCESS_CONTROL_MAX_AGE, "3600");
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
