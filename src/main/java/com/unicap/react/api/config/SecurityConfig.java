3bd2438ba393fb1592d00d4f1307d9038826e0cd
Lucas Matheus Santana Alves <36941834+LucasMatheusSA@users.noreply.github.com>
2020-06-21 17:23:28 -0300

    Ajustes CORS

--- src/main/java/com/unicap/react/api/config/SecurityConfig.java
+++ src/main/java/com/unicap/react/api/config/SecurityConfig.java
@@ -42,6 +42,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(usuarioService)
                 .passwordEncoder(passwordEncoder());
     }
+    
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "content-type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",configuration);
+        return source;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
