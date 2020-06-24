6e9106d90e29a5ff6b2d552b3deb1b6ff5818376
rodrigo.franca <rodrigo_rjsf@hotmail.com>
2020-06-23 21:21:34 -0300

    Finalização implementação S3.

--- src/main/java/com/unicap/react/api/ApiApplication.java
+++ src/main/java/com/unicap/react/api/ApiApplication.java
@@ -1,23 +1,13 @@
 package com.unicap.react.api;
 
-import com.unicap.react.api.service.S3Service;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication implements CommandLineRunner {
-
-    @Autowired
-    private S3Service s3Service;
+public class ApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        s3Service.uploadFile("E:\\Documentos\\Faculdade\\REACT\\Imagens\\distileriesImgs\\caol_ila_original.png");
-    }
 }
